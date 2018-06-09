e076781afa38b8a0a2ef56704df995614e05b34c
quantum13 <quant13@gmail.com>
2018-06-09 06:43:48 +0300

    supplier api

--- backend/src/supplier.hpp
+++ backend/src/supplier.hpp
@@ -21,11 +21,19 @@ public:
             contract(self)
             {}
 
+    // @abi action
     void add_device(account_name device_account, account_name billing_contract, account_name user_account);
 
+    // @abi action
     void add_user(account_name user_account, std::string description);
 
+    // @abi action
     void add_to_balance(account_name user_account, asset quantity);
 
+    // @abi action
     void sub_from_balance(account_name user_account, asset quantity);
+
+
+    // @abi action
+    void device_signal(uint64_t payload);
 };
