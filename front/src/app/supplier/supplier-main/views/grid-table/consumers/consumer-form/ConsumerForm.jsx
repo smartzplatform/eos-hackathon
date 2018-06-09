aebff58736828dfd774da013478a32d15158328f
SlMel <jefferi@ya.ru>
2018-06-10 04:31:07 +0800

    Make functions and some fix

--- front/src/app/supplier/supplier-main/views/grid-table/consumers/consumer-form/ConsumerForm.jsx
+++ front/src/app/supplier/supplier-main/views/grid-table/consumers/consumer-form/ConsumerForm.jsx
@@ -2,7 +2,6 @@ import React, { PureComponent } from "react";
 
 import "./ConsumerForm.less";
 import RegisterForm from "../../../../../../common/register-form/RegisterForm";
-import AppStore from "../../../../../../../store/AppStore";
 import Eos from "../../../../../../../helpers/eos";
 // import { sendTransaction } from "../../../../../../../helpers/eos";
 
@@ -14,27 +13,26 @@ export default class ConsumerForm extends PureComponent {
   }
 
   addConsumer(formData) {
-    const { name, balance } = formData;
+    const { name, balance, description, meta } = formData;
 
-    // AppStore.addConsumer(formData);
     Eos.sendTransaction(
       ["adduser", "addbalance"],
       [
         {
           user_account: name,
-          meta: "",
-          desctiption: ""
+          meta,
+          description
         },
         { user_account: name, quantity: balance }
       ]
     )
       .then(result => {
         console.log(result);
+        this.props.onCloseModal();
       })
       .catch(error => {
         console.error(error);
       });
-    // this.props.onCloseModal();
   }
 
   render() {
@@ -46,11 +44,13 @@ export default class ConsumerForm extends PureComponent {
         name: {
           title: "Name",
           type: "string",
-          minLength: 2,
-          maxLength: 100,
+          minLength: 12,
+          maxLength: 12,
           pattern: "^[a-zA-Z]+$"
         },
-        balance: { title: "Balance", type: "string" }
+        balance: { title: "Balance", type: "string" },
+        description: { title: "Description", type: "string" },
+        meta: { title: "Meta", type: "string" }
       }
     };
 
