2bc04456c89375dbb7e936da59406a1a99490ecb
quantum13 <quant13@gmail.com>
2018-06-09 16:53:48 +0300

    fixes

--- backend/src/debug_tools.hpp
+++ backend/src/debug_tools.hpp
@@ -13,13 +13,14 @@ static constexpr auto header = "============================= ";
 template<typename Arg, typename... Args>
 inline void print_block_start(Arg&& a, Args&&... args) {
     eosio::print(header, "START ", std::forward<Arg>(a), ": ");
-    eosio::print("  ", std::forward<Args>(args)...);
+    // eosio::print("  ", std::forward<Args>(args)...);
     eosio::print("\n");
 }
 
 template<typename Arg, typename... Args>
 inline void print_block_end(Arg&& a, Args&&... args) {
     eosio::print(header, "END ", std::forward<Arg>(a), ": ");
-    eosio::print(std::forward<Args>(args)...);
+    //eosio::print(" ", std::forward<Args>(args)...);
     eosio::print("\n");
 }
+
