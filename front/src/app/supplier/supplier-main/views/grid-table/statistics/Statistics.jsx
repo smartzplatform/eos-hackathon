7c4f844bbb72823ddccb7ce856c4a4747e29c4fd
SlMel <jefferi@ya.ru>
2018-06-10 08:26:28 +0800

    Hot fix

--- front/src/app/supplier/supplier-main/views/grid-table/statistics/Statistics.jsx
+++ front/src/app/supplier/supplier-main/views/grid-table/statistics/Statistics.jsx
@@ -1,51 +1,53 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
 import "./Statistics.less";
 import ButtonAdd from "../../../../../common/button/ButtonAdd";
 import Table from "./../../../../../common/table/Table";
 import Eos from "../../../../../../helpers/eos";
 import AppStore from "../../../../../../store/AppStore";
+import { observer } from "mobx-react";
 
-export default class Statistics extends PureComponent {
+@observer
+export default class Statistics extends Component {
   componentDidMount() {
-    this.timerId = setInterval(() => {
-      Eos.readTable({ code: "supplier", table: "log" })
-        .then(result => {
-          if (
-            result.rows &&
-            Array.isArray(result.rows) &&
-            result.rows.length > 0
-          ) {
-            let statList = [];
+    // this.timerId = setInterval(() => {
+    Eos.readTable({ code: "supplier", table: "log" })
+      .then(result => {
+        if (
+          result.rows &&
+          Array.isArray(result.rows) &&
+          result.rows.length > 0
+        ) {
+          let statList = [];
 
-            result.rows.map(item => {
-              const {
-                log_id,
-                user_account,
-                device_account,
-                balance_diff,
-                final_balance,
-                rate_id
-              } = item;
+          result.rows.map(item => {
+            const {
+              log_id,
+              user_account,
+              device_account,
+              balance_diff,
+              final_balance,
+              rate_id
+            } = item;
 
-              const stat = {
-                logId: log_id,
-                consumer: user_account,
-                device: device_account,
-                balance_diff: balance_diff,
-                balance: final_balance,
-                rate: rate_id
-              };
-              statList.push(stat);
-            });
+            const stat = {
+              logId: log_id,
+              consumer: user_account,
+              device: device_account,
+              balance_diff: balance_diff,
+              balance: final_balance,
+              rate: rate_id
+            };
+            statList.push(stat);
+          });
 
-            AppStore.addStat(statList);
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }, 2000);
+          AppStore.addStat(statList);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    // }, 2000);
   }
 
   componentWillUnmount() {
